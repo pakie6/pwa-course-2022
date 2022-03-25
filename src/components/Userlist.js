483eb72a869539d61d3032211879fed2d7f591fc
pakie6 <pakpon42@hotmail.com>
2022-03-25 18:23:13 +0700

    1610702696-09ex01

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -5,12 +5,13 @@ function Userlist () {
   const [trayStatus, setTrayStatus] = useState("open");
 
   function onButtonClicked() {
-    // ex01 filling logic here
+    onclick = setTrayStatus()
   }
 
   return (
     <div className={"userlist "+trayStatus}>
       <div className="close-button" onClick={onButtonClicked}>x</div>
+      <span Style ={{color:"white"}} > </span>
     </div>
   )
 }
