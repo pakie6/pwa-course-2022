3401f5d4540ff3e2ddfab4d0bf18e9eb8dac8321
pakie6 <pakpon42@hotmail.com>
2022-03-26 03:50:04 +0700

    1610702696

--- src/components/Chatline.js
+++ src/components/Chatline.js
@@ -1,3 +1,4 @@
+import { tab } from '@testing-library/user-event/dist/tab';
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
@@ -15,6 +16,7 @@ function Chatline(props) {
       setElement(<div dangerouslySetInnerHTML={{ __html: html }}></div>);
     }
     else if (text.startsWith("/age")) {
+      // "age"
       const [, name] = text.split(" ");
       fetch("https://api.agify.io/?name=" + name)
         .then(res => res.json())
@@ -27,18 +29,30 @@ function Chatline(props) {
         .then(data => setElement(data.gender));
     }
     else if (text.startsWith("/randomfact")) {
-      // ex10-01 here: Implement randomfact using fetch & promise
-      // https://uselessfacts.jsph.pl/random.json?language=en
-
+      
+      fetch ("https://uselessfacts.jsph.pl/random.json?language=en"+ text)
+      .then(res => res.json())
+      .then(data => setElement(data.id + " => " + data.text[1].data_id));
+      // {"id":"bb38f058-58d7-455c-81af-47a4643b4391",
+      //"text":"The most common name in world is Mohammed.",
+      //"source":"djtech.net",
+      //"source_url":"http:\/\/www.djtech.net\/humor\/useless_facts.htm",
+      //"language":"en",
+      //"permalink":"https:\/\/uselessfacts.jsph.pl\/bb38f058-58d7-455c-81af-47a4643b4391"}
     }
-  };
-
+      else if (text.startsWith("/nationalize")) {
+        const [, name] = text.split(" ");
+        fetch ("https://api.nationalize.io?name=" + name)
+        .then(res => res.json())
+        .then(data => setElement(data.name + " => " + data.country));
+    }
+  {}};
   useEffect(() => {
     transform(text);
   }, []);
 
   return (
-    element
+    <div dangerouslySetInnerHTML={{__html : element}}></div>
   )
 }
 
